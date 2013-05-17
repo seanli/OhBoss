9346f0e6e5d53791558f38994fec3d489dd5ac04
Sean Li <lishang106@gmail.com>
2013-05-17 19:20:59 -0400

    OhBoss environment setup.

--- ohboss/settings_prod.py
+++ ohboss/settings_prod.py
@@ -3,7 +3,7 @@ from settings_base import *
 ENVIRONMENT = 'PROD'
 
 ALLOWED_HOSTS = [
-    'hackbase.herokuapp.com',
+    'ohboss.herokuapp.com',
 ]
 
 DEBUG = False
@@ -12,7 +12,7 @@ DAJAXICE_DEBUG = DEBUG
 
 STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
-AWS_STORAGE_BUCKET_NAME = 'hackbase'
+AWS_STORAGE_BUCKET_NAME = 'ohboss'
 AWS_S3_FILE_OVERWRITE = True
 AWS_QUERYSTRING_AUTH = False
 AWS_HEADERS = {
@@ -20,13 +20,13 @@ AWS_HEADERS = {
 }
 COMPRESS_STORAGE = STATICFILES_STORAGE
 
-STATIC_URL = 'https://s3.amazonaws.com/hackbase/'
-MEDIA_URL = 'https://s3.amazonaws.com/hackbase/'
+STATIC_URL = 'https://s3.amazonaws.com/ohboss/'
+MEDIA_URL = 'https://s3.amazonaws.com/ohboss/'
 
 # Facebook Information
-FACEBOOK_APP_ID = ''
-FACEBOOK_APP_SECRET = ''
+FACEBOOK_APP_ID = '322030074592338'
+FACEBOOK_APP_SECRET = '12136999b94ef4c87acc17dd18e27480'
 
 # AWS Information
-AWS_ACCESS_KEY_ID = ''
-AWS_SECRET_ACCESS_KEY = ''
+AWS_ACCESS_KEY_ID = 'AKIAIWUXQY7UILQNXKDA'
+AWS_SECRET_ACCESS_KEY = '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'
